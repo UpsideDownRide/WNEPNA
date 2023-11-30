6a9d41cafbfab6f82c106893ba2eced0ce387e9b
rn325450 <rn325450@lab.wne.uw.edu.pl>
2023-11-30 17:25:51 +0100

    finished Z5 in class

--- Z5_Nowacki.R
+++ Z5_Nowacki.R
@@ -27,6 +27,20 @@ n <- length(daneZ2)
 logLikPoiss <- \(l) log(l) * sum(daneZ2) - n*l + log(1/prod(sapply(daneZ2, factorial)))
 maxLik::maxNR(logLikPoiss, start=4)
 
+# Zadanie 4
+
+## likelihood - \prod_i=1^n 1/\beta indicator(0:\beta) x_i
+## beta_hat - max(x_i) - 4
+
+dataZ4 <- c(2.13, 1.24, 2, 1.27, 0.04, 2.83)
+n <- dataZ4 |> length()
+beta <- seq(0.01, 5, by = 0.01)
+L <- 1/beta^n
+plot(beta, L, type='l')
+indices <- which(beta < max(dataZ4))
+L[indices] <- 0
+plot(beta, L, type='l')
+
 # Zadanie 7
 
 daneZ7 <- c(3, 4, 3, 2, 7)
@@ -34,3 +48,18 @@ daneZ7 <- c(3, 4, 3, 2, 7)
 likGeom <- \(p) (1 - p)^(sum(daneZ7-1))*p^(length(daneZ7))
 logLikGeom <- \(p) sum(daneZ7 - 1) * log(1 - p) + length(daneZ7) * log(p)
 maxLik::maxNR(logLikGeom, start=0)
+
+# zadanie 9
+
+daneZ9 <- read.csv('./PNA_Z05/Purse_Snatch.txt', header = FALSE)$V1
+nZ9 <- daneZ9 |> length()
+logLikPoissZ9 <- \(l) log(l) * sum(daneZ9) - nZ9*l
+estimateZ9 <- maxLik::maxNR(logLikPoissZ9, start=1)$estimate
+lambdaZ9 <- seq(0, 100, by = 0.01)
+pmfZ9 <- sum(daneZ9) * log(lambdaZ9) - nZ9*lambdaZ9
+
+plot(lambdaZ9, pmfZ9, type = 'l')
+abline(v = estimateZ9, col = 'blue')
+
+qqplot(rpois(nZ9, estimateZ9), daneZ9)
+qqline(daneZ9, distribution=\(p) qpois(p, estimateZ9), probs = c(0.01, 0.99))
