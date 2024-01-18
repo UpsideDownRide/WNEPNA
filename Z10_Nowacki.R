04219525df07fd6527687eee6d6d0ce76997b975
Radomir Nowacki <kenderpl@gmail.com>
2024-01-18 14:48:08 +0100

    Z10

--- Z10_Nowacki.R
+++ Z10_Nowacki.R
@@ -1,3 +1,54 @@
+hipoTestSingleParam <- function(data, fun, start, testValue, alpha = 0.05, twoSided = TRUE) {
+  N <- length(data)
+  alphaAdjusted <- alpha * (1 - twoSided / 2)
+  
+  maxim <- maxLik::maxNR(fun(data), start = start)
+  estimates <- maxim$estimate
+  vcov <- -solve(maxim$hessian)
+  stdErr <- sqrt(vcov)
+  zTest <- (estimates - testValue) / stdErr * sqrt(N)
+  pval <- (twoSided * 2) * (1 - pnorm(abs(zTest), 0, 1))
+  critV <- qnorm(1 - alphaAdjusted, 0, 1)
+  
+  list(`Z-statistic` = zTest, `critical value` = critV, `p-value` = pval, estimates = estimates)
+}
+
+hipoTestingMultiParam <- function(data, fun, weightMatrix, constraintVector, startVector, alpha = 0.05){
+  N <- length(data)
+  maxim <- maxLik::maxNR(fun(data), start = startVector)
+  estimates <- maxim$estimate
+  sigmaHat <- -solve(maxim$hessian)
+  S <- weightMatrix %*% estimates - constraintVector
+  W <- t(S) %*% solve(weightMatrix %*% sigmaHat %*% t(weightMatrix)) %*% S * N
+  
+  ## df = constraints = number of linearaly independent rows of constraint matrix
+  df <- qr(weightMatrix)$rank
+  criticalV <- qchisq(1 - alpha, df = df)
+  pval <- 1 - pchisq(W, df = df)
+  
+  list(`W-statistic` = W, `critical value` = criticalV, `p-value` = pval, estimates = estimates)
+}
+
+# Zadanie 1
+dataZ1 <- read.csv('./PNA_Z10/t1000.csv')$x
+
+funZ1 <- function(data) { function(params) {
+  df <- params[1]
+  N <- length(data)
+  mu <- mean(data)
+  
+  mt <- rbind((data - mu)^2 - df / (df - 2),
+              (data - mu)^4 - 3 * df^2 / ((df - 2) * (df - 4)))
+  Mt <- rowMeans(mt)
+  Wt <- mt %*% t(mt) / N
+  
+  -0.5 * (t(Mt) %*% solve(Wt) %*% Mt)
+}}
+
+## Zbiega się różnie dla różnych wartości startowych, przy 3.5 estimate = 3.19 i odrzucamy
+## przy > 4.5 estimate = 6.3524, z-statistic = 0.68944, p-value = 0.49054, nie odrzucamy H0 df = 6
+hipoTestSingleParam(dataZ1, funZ1, start = 15.5, testValue = 6)
+
 # Zadanie 2 
 dataZ2 <- read.csv('./PNA_Z10/GammaSamp.csv')$x
 funZ2 <- function(data) { function(params){
@@ -17,107 +68,33 @@ funZ2 <- function(data) { function(params){
   -(t(M) %*% solve(W) %*% M)
 }}
 
-RZ2 <- diag(1, 1)
-qZ2 <- c(5)
-
-NZ2 <- length(dataZ2)
-resZ2 <- maxLik::maxNR(funZ2(dataZ2), start = 4)
-#  4.805181
-thetaZ2 <- resZ2$estimate
-sigmaHat <- -solve(resZ2$hessian)/NZ2
-S <- RZ2 %*% thetaZ2 - qZ2
-
-## 1183.108
-W <- t(S) %*% solve(RZ2 %*% sigmaHat %*% t(RZ2)) %*% S * NZ2
-
-vcov <- -solve(resZ2$hessian)
-stdErr <- sqrt(vcov)
-## -0.2365
-zTest <- (resZ2$estimate - 5) / stdErr * sqrt(NZ2)
-pval2 <- 2 * (1 - pnorm(abs(zTest), 0, 1))
-
-critV2 <- qnorm(0.975, 0, 1)
+# Estimate = 4.805196
+# z = -0.2365
+# p-value = 0.8130247
+# Nie ma powodów do odrzucenia hipotezy zerowej alpha = 5
+hipoTestSingleParam(dataZ2, funZ2, start = 4, testValue = 5)
 
 
 # Zadanie 3
 dataZ3 <- read.csv("./PNA_Z10/Norm1000.csv")$x
 
-RZ3 <- diag(1, 2)
-qZ3 <- c(3,1)
-NZ3 <- length(dataZ3)
-resZ3 <- maxLik::maxNR(funZ3alt(dataZ3), start = c(2.5, 1))
-thetaZ3 <- resZ3$estimate
-sigmaHat <- -solve(resZ3$hessian)/NZ3
-S <- RZ3 %*% thetaZ3 - qZ3
-
-## 1183.108
-W <- t(S) %*% solve(RZ3 %*% sigmaHat %*% t(RZ3)) %*% S * NZ3
-
-## df = constraints = number of linearaly independent rows of constraint matrix
-
-# 5.99
-criticalV <- qchisq(0.95, df = 2)
-
-# 0, reject null
-pZ3 <- 1 - pchisq(W, df = 2)
-
-# H0 mi = 3
-
-vcov <- -solve(resZ3$hessian)
-stdErr <- sqrt(vcov[1, 1])
-zTest <- (resZ3$estimate[1] - 3) / stdErr * sqrt(NZ3)
-pval2 <- 2 * (1 - pnorm(abs(zTest), 0, 1))
-
-critV2 <- qnorm(0.975, 0, 1)
-
-funZ3 <- function(data) { function(params){
-  mu <- params[1]
-  sigma <- params[2]
-  n <- length(data)
-  m1 <- data
-  m2 <- (data - mu)^2
-  m3 <- (data - mu)^4
-  z1 <- mu
-  z2 <- sigma^2
-  z3 <- 3 * sigma^4
-  
-  M <- rbind(sum(m1) / n - z1,
-             sum(m2) / n - z2,
-             sum(m3) / n - z3)
-  
-  W <- matrix(0, nrow = 3, ncol = 3)
-  
-  e1 <- m1 - z1
-  e2 <- m2 - z2
-  e3 <- m3 - z3
-  
-  W[1, 1] <- sum(e1^2)
-  W[1, 2] <- sum(e1 * e2)
-  W[1, 3] <- sum(e1 * e3)
-  W[2, 1] <- W[1, 2]
-  W[2, 3] <- sum(e2 * e3)
-  W[3, 1] <- W[1, 3]
-  W[3, 2] <- W[2, 3]
-  W[2, 2] <- sum(e2^2)
-  W[3, 3] <- sum(e3^2)
-  
-  W <- W / n
-  
-  -(t(M) %*% solve(W) %*% M)
-}}
-
-funZ3alt <- function(data) { function(params) {
+funZ3 <- function(data) { function(params) {
   mi <- params[1]
   sigma <- params[2]
   N <- length(data)
   
-  mt <- rbind (data - mi, (data - mi)^2 - sigma^2, (data - mi)^4 - 3 * sigma^4)
+  mt <- rbind(data - mi, (data - mi)^2 - sigma^2, (data - mi)^4 - 3 * sigma^4)
   Mt <- rowMeans(mt)
-  Wt <- mt%*%t(mt) * 1 / N
+  Wt <- mt %*% t(mt) * 1 / N
   -0.5 * (t(Mt) %*% solve(Wt) %*% Mt)
 }}
 
-maxLik::maxNR(funZ3alt(dataZ3), start = c(2.5, 1))$estimate
+
+# W = 1.183, critical value = 5.99, p-value = 0.5534, nie ma powodów do odrzucenia H0 mu = 3, sigma = 1
+hipoTestingMultiParam(dataZ3, funZ3, weightMatrix = diag(1, 2), constraintVector = c(3, 1), startVector = c(2.5, 1))
+
+# Z = 1.069, critical value = 1.96, p-value = 0.285, nie ma powodów do odrzucenia H0 mu = 3
+hipoTestSingleParam(dataZ3, funZ3, start = c(2.5, 1), testValue = 3)
 
 #### Zadanie 4
 
